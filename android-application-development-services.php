402f36872fefb82f665ad06526d79f4af84dcbf2
varunsin <varunsin001@gmail.com>
2013-01-20 19:41:28 +0530

    Android page
    Android app page

---
+++ android-application-development-services.php
@@ -0,0 +1,105 @@
+<?php
+include('core.php');
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1"/>
+<title>Xenial Technologies</title>
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<?php include($ABSOLUTE_URL.'/includes/analyticstracking.php'); ?>
+</head>
+<body>
+
+<div id="header-inner">
+    <?php include($ABSOLUTE_URL.'/includes/header_innerpages.php'); ?>
+</div>
+
+
+
+<div id="content">
+
+  <div class="inner_content margin-t-min-35" style=" border-top-left-radius:5px; border-top-right-radius:5px;">
+
+  <div class="flt sub-menu margin-t-80" style="width:290px; margin-left:15px;">
+
+        <ul>
+            <li><a href="website_design.php">Website Design</a></li>
+            <li><a href="web_application_development.php" >Web Application Development</a></li>
+            <li><a href="e-commerce.php" >E-Commerce</a></li>
+            <li><a href="content_management_system.php">Content Management System</a></li>
+            <li><a href="testing_&_qa.php">Testing & QA</a></li>
+            <li><a href="#">3D & Animation</a></li>
+            <li><a href="internet_marketing.php">Internet Marketing</a></li>
+            <li><a href="iphone-application-development.php" class="current">iPhone App Development</a></li>
+            <li><a href="android-application-development.php">Android App Development</a></li>
+            <li><a href="software-development.php">Software Development</a></li>
+        </ul>
+
+	</div>
+
+   <div class="flt wid675">
+
+    <p>&nbsp;</p>
+
+    <span class="headings">Android Application Development</span>
+
+    <p>Android smartphones have a current market share of approximately 75% worldwide, with 500 million devices activated in total and 1.3 million activations per day. The operating system's success has made it a target for online business owners as a part of their business expansion strategy.</p>
+<p>At Xenial, we have experienced Android application developers with years of experience in mobile application development technology and are well conversant with Android Software Development Kit (SDK), OpenGL, 3D graphics, Android Security Architecture and other technologies required building best in class Android apps.</p>    
+      <p><strong>Our Android development Services  includes:</strong></p>
+      <ul class="small-list" type="disc"><ul type="disc">
+        <li>Development of Custom <strong>Android Applications </strong></li>
+        <li>Development of Android <strong>Gaming Applications</strong></li>
+        <li>Android <strong>Multimedia Application </strong>Development</li>
+        <li>Development of Android <strong>Enterprise Applications </strong></li>
+        <li><strong>Testing</strong> Android Application </li>
+        <li><strong>Maintenance</strong> and <strong>Upgrade</strong> Android Application </li>
+      </ul>
+    </li>
+    </ul>
+    <p>According to marketing research in October'12, there were approximately 700,000 apps available for Android, and the estimated number of applications downloaded from Google Play, Android's primary app store, was 25 billion.</p>
+      <p><strong>Why Xenial: </strong></p>     
+       <ul class="small-list" type="disc"><ul type="disc">
+        <li>24/7 Constant Communication </li>
+        <li>Professional Support </li>
+        <li>Faster Service Assurance </li>
+        <li>Regular Showcase Of Work </li>
+        <li>Quality Network </li>
+        <li>Client Satisfaction </li></ul>
+      </ul>
+      <p> We can help you develop a light weight and interactive Android app that will fulfill your online business needs yet simple to operate and smooth to work with. We can design any android app from native android applications to android games to android based enterprise applications.</p>
+<p>Contact us today to help us build your dream Android App.</p>
+</div>
+
+        
+
+    <div class="clear"></div>
+
+    <div class="spacer20"></div>
+
+    <div class="clear"></div>
+
+
+
+  </div>
+
+</div>
+
+<div class="clear"></div>
+<div class="footer-upper">
+    <?php include($ABSOLUTE_URL.'/includes/upper_footer.php'); ?>
+</div>
+<div class="clear"></div>
+<div class="footer">
+    <?php include($ABSOLUTE_URL.'/includes/lower_footer.php'); ?>
+</div>
+
+<div class="clear"></div>
+
+
+
+</body>
+
+</html>
